d8f53be7aa1f68e55162ec2bd4918b55460e8bcd
alangudi417 <211910311+alangudi417@users.noreply.github.com>
2026-02-11 16:53:17 -0600

    redoing function

--- src/null_columns.py
+++ src/null_columns.py
@@ -3,11 +3,18 @@ import pandas as pd
 # This function helps to limit the result only to columns with null values per dataset
 def show_null_columns(df, df_name="Dataset"):
     null_counts = df.isnull().sum()
-    null_counts = null_counts[null_counts > 0]
+    null_percent = (df.isnull().mean() * 100)
 
-    print(f"\nNull values in {df_name}:")
-    
-    if null_counts.empty:
-        print("No null values found.")
+    null_summary = pd.DataFrame({
+        "null_count": null_counts,
+        "null_percent": null_percent
+    })
+
+    null_summary = null_summary[null_summary['null_count'] > 0]  # Columns with null values only
+    null_summary = null_summary.sort_values('null_count', ascending=False)  # Sort by highest null
+
+    print(f"\nColumns with null values in {df_name}:")
+    if null_summary.empty:
+        print("No null values found")
     else:
-        print(null_counts.sort_values(ascending=False))
+        print(null_summary)
